1cb104aeb7e27384c158c6c1ae0f35136127a24b
vunam2000 <vunam722000@gmail.com>
2021-12-29 14:42:10 +0700

    add env

--- config/constants.py
+++ config/constants.py
@@ -1,9 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 class InfluxDBConstant:
-    HOST = '127.0.1.1'
-    PORT = '8086'
-    USERNAME = 'admin'
-    PASSWORD = '123456'
-    DATABASE_MONITORING = 'monitoring_db'
+    HOST = os.getenv("INFLUX_HOST") or '165.22.52.226'
+    PORT = os.getenv("INFLUX_PORT") or '8086'
+    USERNAME = os.getenv("INFLUX_USERNAME") or 'admin'
+    PASSWORD = os.getenv("INFLUX_PASSWORD") or '123456'
+    DATABASE_MONITORING = os.getenv("DATABASE_MONITORING") or 'monitoring_db'
 
 class Constant:
     CAPACITY_THRESHOLD = 53687091200
